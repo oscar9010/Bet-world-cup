f2553e8c73d1468f0dbe25c4927f9b1f230e06e6
Oscar C <oscar.canon@holafly.com>
2026-05-27 23:11:09 +0200

    Agregar credenciales Firebase de farmamodelo

--- farmamodelo/config.js
+++ farmamodelo/config.js
@@ -4,14 +4,14 @@ const APP_CONFIG={
   badge:"MUNDIAL 2026",
   loadingText:"Cargando partidos...",
   firebase:{
-    // TODO: reemplazar con credenciales del proyecto Firebase de farmamodelo
-    apiKey:"REEMPLAZAR",
-    authDomain:"REEMPLAZAR",
-    databaseURL:"REEMPLAZAR",
-    projectId:"REEMPLAZAR",
-    storageBucket:"REEMPLAZAR",
-    messagingSenderId:"REEMPLAZAR",
-    appId:"REEMPLAZAR"
+    apiKey: "AIzaSyA8D4lBQD__dUIJ_HLYPBdXvpVIa-sgTdI",
+    authDomain: "farmamodelo.firebaseapp.com",
+    databaseURL: "https://farmamodelo-default-rtdb.firebaseio.com",
+    projectId: "farmamodelo",
+    storageBucket: "farmamodelo.firebasestorage.app",
+    messagingSenderId: "1059253136120",
+    appId: "1:1059253136120:web:e2b4833d4ed3d75fc7a90d",
+    measurementId: "G-3VJ2XN4FS4"
   },
   theme:{
     primary:"#0ea5e9",
